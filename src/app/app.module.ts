49a921b8970984048f21d20d23a42b790673fbdd
IvBrt <crstn.b7@gmail.com>
2022-05-24 20:54:42 -0500

    ruteado de vista principal y de  vista de notas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routing'; 
 
 import { AppComponent } from './app.component';
+import { MyNotesComponent } from './my-notes/my-notes.component';
+import { AccountComponent } from './account/account.component';
+import { NotesComponent } from './notes/notes.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNotesComponent,
+    AccountComponent,
+    NotesComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
